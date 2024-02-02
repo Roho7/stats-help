5a1a5ea5cdb7f84b54ef1e4e25ce24e4abfef49e
Roho7 <rohosen2@gmail.com>
2024-02-02 16:27:56 +0000

    :add [miro-board]

---
+++ app/bigdawgs/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <iframe
+        width="768"
+        height="432"
+        src="https://miro.com/app/live-embed/uXjVNxlkxmo=/?moveToViewport=-80957,-70126,172068,121598&embedId=837964518428"
+        scrolling="no"
+        allow="fullscreen; clipboard-read; clipboard-write"></iframe>
+    </div>
+  );
+}
+
+export default page;
